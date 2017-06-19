87d0085b8107be52b5f874211b454b7c35ce7757
Steven Weaver <sweaver@temple.edu>
2017-06-19 15:03:50 -0400

    updating main.py

--- main.py
+++ main.py
@@ -2,61 +2,44 @@ import requests
 import datetime
 from pymongo import MongoClient
 import json
+import itertools
+
+
+with open('config.json') as config_h:
+    config = json.load(config_h)
+    symbols = config['symbols']
+    db = config['db']
 
 threshold = 10
-
-with open('ticker.json') as myTicker:
-    symbols = json.load(myTicker)['symbols']
-
+client = MongoClient(db['host'], db['port'])
+db = client[db['name']]
+api_url = "https://min-api.cryptocompare.com/data/pricemultifull?"
 
 timedeltas = [5, 10, 30, 60, 120, 240, 480, 960, 1920, 3840, 7680]
-
-
-
-client = MongoClient('localhost', 27017)
-
-db = client['financial']
 collection  = 'ticker'
-'''
-for s in symbols:
-    res = requests.get('https://min-api.cryptocompare.com/data/price?fsym={0}&tsyms=USD'.format(s))
-    r = res.json()
-    time = datetime.datetime.now()
-    
-    entry = {'timestamp': time , 'ticker': s, 'price': float(r['USD'])}
 
-    for t in timedeltas:
-        r = db[collection].find({'ticker': s, 'timestamp':{'$gte': (time - datetime.timedelta(minutes=t)) }} ).sort('timestamp').limit(1)
-        for x in r:
-            entry['*%delta -{0}min'.format(str(t))] = (entry['price'] - x['price'])/entry['price'] * 100
-            x['*%delta +{0}min'.format(str(t))] = (entry['price'] - x['price'])/x['price'] * 100
-            db[collection].update({'_id': x['_id']}, x)
-    
-    db[collection].insert_one(entry)
-'''
+def format(entry):
+	# TODO : mongo should have its own created timestamps
+	time = datetime.datetime.now()
+	name = entry[0]
+ 	price = float(entry[1]['PRICE'])
+ 	lastVolumeTo = float(entry[1]['LASTVOLUMETO'])
+ 	lastVolume = float(entry[1]['LASTVOLUME'])
 
-while symbols:
-    if len(symbols) > threshold:
-        temp = symbols[0:threshold]
-        symbols = symbols[threshold :]
-    else:
-        temp = symbols[0:]
-        symbols = []
-    print (temp)
-    res = requests.get('https://min-api.cryptocompare.com/data/price?fsym=USD&tsyms={0}'.format(','.join(temp)))
-    newData = res.json()
-
-    for k in newData:
-        time = datetime.datetime.now()
-
-        entry = {'timestamp': time , 'ticker': k, 'price': 1./float(newData[k])}
-        for t in timedeltas:
-            r = db[collection].find({'ticker': k, 'timestamp':{'$gte': (time - datetime.timedelta(minutes=t)) }} ).sort('timestamp').limit(1)
-            for x in r:
-                entry['*%delta -{0}min'.format(str(t))] = (entry['price'] - x['price'])/entry['price'] * 100
-                x['*%delta +{0}min'.format(str(t))] = (entry['price'] - x['price'])/x['price'] * 100
-                db[collection].update({'_id': x['_id']}, x)
-
-        db[collection].insert_one(entry)
+	entry = {
+			'timestamp': time ,
+			 'ticker': entry[0],
+			 'price': price,
+			 'lastVolumeTo' : lastVolumeTo,
+			 'lastVolue' : lastVolume
+			}
+	return entry
 
 
+# Split ticker into groups of threshold in order to play nice with cryptocompare server
+args = [iter(symbols)] * threshold
+symbols_for_dispatch = itertools.izip_longest(fillvalue='', *args)
+urls = [api_url + 'fsyms=USD&tsyms={0}'.format(','.join(temp)) for temp in symbols_for_dispatch]
+
+data = list(itertools.chain.from_iterable([requests.get(url).json()["RAW"]['USD'].items() for url in urls]))
+[db[collection].insert_one(format(entry)) for entry in data]
